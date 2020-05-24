cc31bf071cc0c15f26170bb0ac1153cf7215bd90
Josh Long <josh@joshlong.com>
2020-05-24 03:44:47 -0700

    the delete api endpoint seems to work - that is a delete followed by a read returns 0 results. but the uri is there in the pinboard API, plain as day. and i have no idea why.

--- src/test/kotlin/pinboard/webclient/PinboardClientTest.kt
+++ src/test/kotlin/pinboard/webclient/PinboardClientTest.kt
@@ -18,7 +18,7 @@ class PinboardClientTest {
   private val testTag = "pbctest"
   private val testTag2 = "pbctest2"
   private val bookmark = Bookmark("http://garfield.com", "description",
-      "extended", "hash", "meta", Date(), true, true, arrayOf(this.testTag, this.testTag2))
+      "extended", "hash", "meta", Date(), false, true, arrayOf(this.testTag, this.testTag2))
 
   private val pinboardClientTestTags: Array<String> by lazy {
     bookmark.tags
